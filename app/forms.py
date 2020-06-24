27729c71626d7b911b22543eac5c08fc65f01841
f-westergren <folke.westergren@gmail.com>
2020-06-24 12:49:35 -0400

    fix typo

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
-	email = StringField('Username', validators=[DataRequired(), Email(message='Enter a valid email')])
+	email = StringField('Email', validators=[DataRequired(), Email(message='Enter a valid email')])
 	password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
 	remember_me = BooleanField('Remember Me')
 	
