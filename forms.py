673d967c9d0fcdd09e4fa967f522b23742d8a4d6
f-westergren <folke.westergren@gmail.com>
2020-06-24 08:12:42 -0400

    login form and login functionality

--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
+	email = StringField('Username', validators=[DataRequired(), Email(message='Enter a valid email')])
+	password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
+	confirm = PasswordField('Re-enter Password', validators=[DataRequired(), EqualTo('password')])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+	
