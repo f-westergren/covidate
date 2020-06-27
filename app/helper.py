435c0aace4ce57ff9a92d06f430d7339e4e033d6
f-westergren <folke.westergren@gmail.com>
2020-06-26 22:29:22 -0400

    add get_covid_data to helper

--- app/helper.py
+++ app/helper.py
@@ -1,8 +1,10 @@
 import requests
 from app.secrets import MAPQUEST_API_KEY
 from app.states import states
+from datetime import datetime
 
 BASE_MAP_API_URL = 'http://www.mapquestapi.com/geocoding/v1'
+BASE_COVID_API_URL = 'https://corona.lmao.ninja/v2/historical/usacounties'
 
 def get_state_and_county(location):
   """ Get locations from api from input location"""
@@ -14,27 +16,50 @@ def get_state_and_county(location):
   except Error:
     return Error
 
-  data = res.json()['results'][0]
-
-  locations = []
+  data = res.json()['results'][0]['locations'][0]
 
   # Check to see if country is US, if so, add to locations.
-  for place in data['locations']:
-    country = place['adminArea1']
+  country = data['adminArea1']
+  state = data['adminArea3']
+  county = data['adminArea4']
 
-    if country == 'US' and place['geocodeQuality'] != 'COUNTRY':
-      state = place['adminArea3']
-      county = place['adminArea4']
+  # Strip 'County' from county name
+  county_name = county[0:-7] if county[-6:] == 'County' else county
 
-      # Strip 'County' from county name
-      county_name = county[0:-7] if county[-6:] == 'County' else county
+  # Only return if location is in US.
+  if country == 'US' and data['geocodeQuality'] != 'COUNTRY':
+    return {'state': states[state], 'county': county_name}
 
-      # Only add if result exists and change state code to name.
-      if state != '' and county_name != '':
-        locations.append({'state': states[state], 'county': county_name})
+  # Otherwise return error string.
+  return "Can't find location in USA."
+    
+
+def get_covid_data(date, state, county):
+  """ Get data from COVID-api and return array of dates with data"""
+
+  date = datetime.strptime(date, '%Y-%m-%d')
+  diff = datetime.now() - date
+  days = diff.days
+
+
+  res = requests.get(f'{BASE_COVID_API_URL}/{state}', params={'lastdays': days})
+
+  for c in res.json():
+    if c['county'] == county:  
+      return c['timeline']
+
+  return res
   
-  # If no location, return error.
-  if len(locations) == 0:
-    return "Can't find location in USA."
 
-  return locations
+
+
+# Räkna ut dagar att requesta
+  # dagens datum minuns requestat datum
+  # det här blir parameter lastdays
+
+# Requesta med stat som stat och lastdays som lastsdays
+
+# Ta ut resultat beroende på vilket county.
+
+# Lägg resultatet med datumen i en array?
+
