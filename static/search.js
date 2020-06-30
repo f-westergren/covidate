4cac02d48944323299a907588725dc7c4a668366
f-westergren <folke.westergren@gmail.com>
2020-06-29 21:26:52 -0400

    c3js charts

--- static/search.js
+++ static/search.js
@@ -1,18 +1,43 @@
+// global currentUser variable
+let currentSearch = null
+let chart = null
+let showAll = false
+
 async function processForm(e) {
   e.preventDefault()
   const location = document.querySelector('#search-input').value
   const date = document.querySelector('#date').value
-  console.log(location, date)
-  try {
-    res = await axios.post('http://localhost:5000/search', {
-      "location": location,
-      "date": date
-  })
 
-  console.log(res)
-  } catch (err) {
-    console.log(err)
+  try {
+  const searchInstance = await Search.create(location, date)
+  currentSearch = searchInstance
+  } catch (e) {
+    console.log(e)
   }
+
+  chart = currentSearch.generateGraph('#cases-chart')
+  document.querySelector('#show-btn').classList.remove('d-none')
 }
 
 document.querySelector('#search-form').addEventListener("submit", processForm)
+
+document.querySelector('#show-btn').addEventListener('click', (e) => {
+  if (!showAll) {
+    currentSearch.showAllDates(chart)
+    e.target.classList.remove('btn-primary')
+    e.target.classList.add('btn-outline-primary')
+  } else {
+    currentSearch.generateGraph('#cases-chart')
+    e.target.classList.remove('btn-outline-primary')
+    e.target.classList.add('btn-primary')
+  }
+  showAll = !showAll
+})
+
+document.querySelector('#death-btn').addEventListener('click', () => {
+  chart.load({
+    columns: [
+        ['deaths', ...currentSearch.deaths]
+    ]
+});
+})
